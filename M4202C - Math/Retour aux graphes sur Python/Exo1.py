19bf84efcf3a4a2eb5fa401a05282221833db8c2
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-01-25 15:16:19 +0100

    Update Exo1.py

--- M4202C - Math/Retour aux graphes sur Python/Exo1.py
+++ M4202C - Math/Retour aux graphes sur Python/Exo1.py
@@ -1,4 +1,5 @@
 mat1=[[0,0,0,1],[0,0,1,0],[0,0,1,1],[1,1,1,1]]
+G={1:[1,2],2:[2,3,4],3 :[],4 :[2]}
 
 def succ(mat,som):
     listSucc=[]
@@ -28,7 +29,14 @@ def nb_pred(mat,som):
                 nb=nb+1
     return nb
 
-print(succ(mat1,2))
-print(nb_succ(mat1,2))
-print(pred(mat1,2))
-print(nb_pred(mat1,2))
+#print(succ(mat1,2))
+#print(nb_succ(mat1,2))
+#print(pred(mat1,2))
+#print(nb_pred(mat1,2))
+
+def succD(dico,som):
+    listSucc=[]
+    listSucc.append(dico[som])
+    return listSucc
+
+print(succD(G,2))
