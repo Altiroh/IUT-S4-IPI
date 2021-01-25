977f9322d11ae9f416fd446597503a7aac24b2fe
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-01-25 14:43:25 +0100

    Update Exo1.py

--- M4202C - Math/Retour aux graphes sur Python/Exo1.py
+++ M4202C - Math/Retour aux graphes sur Python/Exo1.py
@@ -6,3 +6,14 @@ def succ(mat,som):
             if mat[som][i]==1: #vérifiacation de l'existence d'un successeur
                 listSucc.append(i)
     return listSucc
+
+def nb_succ(mat,som):
+    nb=0
+    for i in range (len(mat[som])):
+            if mat[som][i]==1: #vérifiacation de l'existence d'un successeur
+                nb=nb+1
+    return nb
+
+print(succ(mat1,2))
+print(nb_succ(mat1,2))
+
