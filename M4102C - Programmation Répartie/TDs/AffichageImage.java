d901a8d5c4f20351023f71737cbb76461de5cbc5
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-02-10 15:46:42 +0100

    Update AffichageImage.java

--- M4102C - Programmation Répartie/TDs/AffichageImage.java
+++ M4102C - Programmation Répartie/TDs/AffichageImage.java
@@ -1,5 +1,5 @@
 import javax.swing.JFrame; //Fenêtre
-
+import javax.*
 import javax.swing.JLabel; //Affichage de l'image
 import javax.swing;ImageIcon; //Image
 import java.awt.GridLayout; //Placement en tableau 1x1
@@ -28,4 +28,45 @@ public static void main(String argv[])
 {   //Démarrage du programme
     new AffichageImage("Elephant.gif"); //Créatiopn de la fenêtre
     //new AffichageImage("Koala.gif");
+}
+
+public class AfficheImmage{ // Interface => hérite de JFRAME
+
+    private JLabel dessin;
+    private 
+public Presentation(){
+    super(monImage); //Nom du fichier image en titre
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//arrêt du programme si fermeture
+    //GridLayout placeur = new GridLayout(1,1,2,2); //Placement en tableau(GridLayout)
+    FlowLayout placeur = new FlowLayout(FlowLayout.CENTER, 20, 40); //Placement FlowLayout
+    getContentPane().setLayout(placeur);//PLacement dans le conteneur de la fenêtre
+
+    dessin = new JLabel(new ImageIcon("Elephant.gif"));
+    explications = new JTextArea("Explications :\n",10,20);
+
+    JScrollPane asc = new JscrollPane(explications, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+    JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+    JSplitePane divise = new JSplitePane(JSplitePane.HORIZONTAL_SPLITE, dessin, asc);
+    getContentPane().add(divise, BorderLayout.CENTER);
+
+    JPanel bas = new JPanel();
+    bas.setLayout(new GridLayout(1,2,5,5));
+    choix = new JComboBox<String>;
+    choix.addItem("Koala");
+    choix.addItem("Elephant");
+    bas.add(choix);
+    auteur = new JTextField("Auteur : ");
+    bas.add(auteur);
+    getContentPane().add(bas, BorderLayout.SOUTH);
+
+    pack();
+    setVisible(true);
+}
+
+public static void main(String argv[])
+{   //Démarrage du programme
+    new Presentation();
+}
+
 }
